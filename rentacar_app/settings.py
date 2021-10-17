05c8ecf7d2723b16700a50fc8c4cf8a697edd65c
Muadib07 <loken070707@gmail.com>
2021-10-17 23:52:39 +0200

    route problem

--- rentacar_app/settings.py
+++ rentacar_app/settings.py
@@ -151,8 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    "C:/Users/mateu/OneDrive/Desktop/projekt2.0Git/Rentacar-business-app/rentacar_app/static",
 ]
 #'C:/projects/DjangoProjects/Tasker/mattask/task_view/static',
 
--- static_cdn/js/script.js
+++
@@ -1,7 +0,0 @@
-$(window).on("scroll", function () {
-    if ($(window).scrollTop()) {
-        $('.navbar').addClass('half-white-on-scroll');
-    } else {
-        $('.navbar').removeClass('half-white-on-scroll');
-    }
-});
