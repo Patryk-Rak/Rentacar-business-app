efb8c27f317ce8834fa0d72d8f60da0e7275865d
Muadib07 <loken070707@gmail.com>
2021-10-28 11:50:39 +0200

    heroku problem with database

--- rentacar_app/settings.py
+++ rentacar_app/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'rentacar_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rental_car_app',
+        'NAME': 'car_app_database',
         'USER': 'postgres',
         'PASSWORD': 'Base_Manager0!7',
         'HOST': '127.0.0.1',
