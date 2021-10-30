4f7e81d8519c0bd2cf9c56b520b1ba28b2368bae
Muadib07 <loken070707@gmail.com>
2021-10-30 18:48:58 +0200

    heroku last fight in midnight II,5

--- rentacar_app/settings.py
+++ rentacar_app/settings.py
@@ -30,9 +30,8 @@ SECRET_KEY = "chomik"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['heroku-car-rental-app-smp.herokuapp.com',
-                 '127.0.0.1']
-#ALLOWED_HOSTS = ["rentacar_app.herokuapp.com"]
+ALLOWED_HOSTS = ['rentacar-heroku.herokuapp.com']
+
 
 
 # Application definition
